375b1e200558eff9771919e2995312ef1e0f4acb
张利 <zhangli@aidaijia.com>
2016-12-13 18:13:42 +0800

    删除demo中normalize.css

--- dist/js/jquery.SmallEditor.js
+++ dist/js/jquery.SmallEditor.js
@@ -16,7 +16,7 @@
     Editor.VERSION = '0.1.0';
 
     Editor.DEFAULTS = {
-        tools: ['bold', 'italic', 'list-ol', 'list-ul'],
+        tools: ['bold', 'italic', 'underline', 'strikethrough', 'list-ol', 'list-ul'],
         activeClass: 'active',
         placeholder: '请输入内容…'
     }
@@ -28,30 +28,34 @@
         //var tools = options.tools;
         //TODO 自定义工具栏
         var EDITOR_HTML = "<div class='editor-container'>";
-        var TOOLBAR_HTML = "<div class='editor-container'><div class='editor-toolbar'><ul>";
-        TOOLBAR_HTML += "<li class='editor-btn'><a href='#' title='加粗(Ctr + B)' data-command='bold'><i class='fa fa-bold'></i></a></li>";
-        TOOLBAR_HTML += "<li class='editor-btn'><a href='#' title='斜体(Ctr + I)' data-command='italic'><i class='fa fa-italic'></i></a></li>";
-        TOOLBAR_HTML += "<li class='editor-btn'><a href='#' title='下划线(Ctr + U)' data-command='underline'><i class='fa fa-underline'></i></a></li>";
-        TOOLBAR_HTML += "<li class='editor-btn'><a href='#' title='删除线' data-command='strikethrough'><i class='fa fa-strikethrough'></i></a></li>";
-        //TOOLBAR_HTML += "<li><div class='separator'></div></li>";
-        TOOLBAR_HTML += "<li class='editor-btn'><a href='#' title='有序列表' data-command='insertOrderedList'><i class='fa fa-list-ol'></i></a></li>";
-        TOOLBAR_HTML += "<li class='editor-btn'><a href='#' title='无序列表' data-command='insertUnorderedList'><i class='fa fa-list-ul'></i></a></li>";
-        TOOLBAR_HTML += "</ul></div>";
-
+        var TOOLBAR_HTML = "<div class='editor-container'><div class='editor-toolbar'>";
+        TOOLBAR_HTML += "<div class='editor-btn'><a href='#' title='加粗(Ctr + B)' data-command='bold'><i class='fa fa-bold'></i></a></div>";
+        TOOLBAR_HTML += "<div class='editor-btn'><a href='#' title='斜体(Ctr + I)' data-command='italic'><i class='fa fa-italic'></i></a></div>";
+        TOOLBAR_HTML += "<div class='editor-btn'><a href='#' title='下划线(Ctr + U)' data-command='underline'><i class='fa fa-underline'></i></a></div>";
+        TOOLBAR_HTML += "<div class='editor-btn'><a href='#' title='删除线' data-command='strikethrough'><i class='fa fa-strikethrough'></i></a></div>";
+        TOOLBAR_HTML += "<div class='separator'></div>";
+        TOOLBAR_HTML += "<div class='editor-btn'><a href='#' title='引用' data-command='blockquote'><i class='fa fa-quote-left'></i></a></div>";
+        TOOLBAR_HTML += "<div class='editor-btn'><a href='#' title='有序列表' data-command='insertOrderedList'><i class='fa fa-list-ol'></i></a></div>";
+        TOOLBAR_HTML += "<div class='editor-btn'><a href='#' title='无序列表' data-command='insertUnorderedList'><i class='fa fa-list-ul'></i></a></div>";
+        TOOLBAR_HTML += "</div>";
         EDITOR_HTML += TOOLBAR_HTML;
-        EDITOR_HTML += "<div class='editor-body-wrap'><div class='editor-body' contenteditable='true' placeholder='" + options.placeholder + "'></div></div>";
+        EDITOR_HTML += "<div class='editor-body-wrap'><div class='editor-body' contenteditable='true' placeholder='" + options.placeholder + "'>" + element.val() +" </div></div>";
 
         var _editor = $(EDITOR_HTML);
         element.after(_editor);
         element.hide();
-
+        _editor.find(".editor-body").focus();
         //Toolbar 点击处理
         _editor.on('click', '[data-command]', function (event) {
             event.preventDefault();
             var command = $(this).data("command");
             if (command) {
                 _editor.find(".editor-body").focus();
-                document.execCommand(command, false, null);
+                if (command === 'blockquote') {
+                    document.execCommand('formatBlock', false, command);
+                }else {
+                    document.execCommand(command, false, null);
+                }
                 _this.detectState();
             }
         });
@@ -62,11 +66,11 @@
         })
 
 
-        _editor.on('keyup mouseup', '.editor-body', function(event) {
+        _editor.on('keyup mouseup', '.editor-body', function (event) {
             _this.detectState();
         })
 
-        _editor.on('paste', '.editor-body', function(event) {
+        _editor.on('paste', '.editor-body', function (event) {
 
         })
 
@@ -78,8 +82,9 @@
         var _this = this, _options = _this.optinos, activeClass = _options.activeClass;
         _this.editor.find("[data-command]").each(function (index, element) {
             var command = $(element).data("command");
+
             //console.log("==========> has command " + command + "?" + document.queryCommandState(command) )
-            if (document.queryCommandState(command) === true) {
+            if (_this.isStateOn(command)) {
                 $(element).addClass(activeClass);
             } else {
                 $(this).removeClass(activeClass);
@@ -87,17 +92,50 @@
         })
     }
 
+    Editor.prototype.isStateOn = function(commmand) {
+        var _this = this;
+        if (document.queryCommandState(commmand) === true) {
+            return true;
+        }else {
+            if (_this.commonAncestorContainer()) {
+                return $(this.commonAncestorContainer()).closest(commmand).length !== 0;
+            }
+            return false;
+        }
+    }
+
+    Editor.prototype.commonAncestorContainer = function() {
+        var selection = document.getSelection();
+        if (selection.rangeCount !== 0) {
+            return selection.getRangeAt(0).commonAncestorContainer;
+        }
+    }
+
+    Editor.prototype.selectContents = function(contents) {
+        var end, range, selection, start;
+        selection = window.getSelection();
+        range = selection.getRangeAt(0);
+        start = contents.first()[0];
+        end = contents.last()[0];
+        range.setStart(start, 0);
+        range.setEnd(end, end.childNodes.length || end.length);
+        selection.removeAllRanges();
+        return selection.addRange(range);
+    };
+
     //设置编辑器段落分割标签
     Editor.prototype.setDefaultParagraphSeparator = function () {
         document.execCommand('defaultParagraphSeparator', false, 'p');
     }
 
     Editor.prototype.change = function (editor) {
-
+        this.element.val(editor.find('div.editor-body').html());
     }
 
     $.fn.smallEditor = function (options) {
-        return new Editor(this, options)
+        return this.each(function(){
+            new Editor(this, options)
+        })
     }
 
 }(jQuery);
